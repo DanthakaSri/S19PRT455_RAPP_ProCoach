ee35e3f7cb24115022423e401bfc60ce8a14b06c
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-08-20 10:28:01 +0930

    Creating Context

--- BookStoreApp/BookStoreApp/DAL/Printer.cs
+++ BookStoreApp/BookStoreApp/DAL/Printer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,19 @@ namespace BookStoreApp.DAL
 {
     public class Printer
     {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Brand { get; set; }
+    }
+
+
+    public class EfBridgeContext : DbContext
+    {
+        public EfBridgeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Printer> printer { get; set; }
+
     }
 }
