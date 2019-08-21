fe22c5edaa9ed12d62024a2515bb47543c634e63
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-08-21 14:12:14 +0930

    Adding new class scanner

---
+++ BookStoreApp/BookStoreApp/DAL/Scanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.DAL
+{
+    public class Scanner
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Model { get; set; }
+        public String Brand { get; set; }
+    }
+}
