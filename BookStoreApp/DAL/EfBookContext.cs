c865196c9dad6044ec4166fed5631e40ea8c7019
prabhat_1234 <kprabhatrauniyar1@gmail.com>
2019-08-21 12:13:47 +0930

    creating context

---
+++ BookStoreApp/DAL/EfBookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.DAL
+{
+    public class EfBookContext: DbContext
+
+    {
+        public EfBookContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
