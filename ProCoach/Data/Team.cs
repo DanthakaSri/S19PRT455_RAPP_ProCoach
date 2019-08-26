c96ed7113dbdf02f3ae2dd2c76534fd1be2e1b94
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-08-26 13:56:21 +0930

    Defining variables in team Class

---
+++ ProCoach/Data/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCoach.Data
+{
+    public class Team
+    {
+        public  String  Name  { get; set; }
+
+        public String Email { get; set; }
+
+        public String Club { get; set; }
+
+        public String Sport { get; set; }
+
+        public String Competition { get; set; }
+
+       
+
+        
+
+    }
+}
