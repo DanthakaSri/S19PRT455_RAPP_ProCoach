3a206b385f4f8dbac2ecae579379f82e152467a7
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-09-04 17:31:54 +0930

    defining the variables for practice_session

---
+++ ProCoach/Data/practice_session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCoach.Data
+{
+    public class practice_session
+    {
+        public int id { get; set; }
+
+        public int team_id { get; set; }
+
+        public int coach_id { get; set; }
+
+        public string address { get; set; }
+
+    }
+}
