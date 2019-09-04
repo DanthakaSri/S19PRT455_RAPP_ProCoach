ea82fb55593df64b85e10bd161162f322694f047
raj20 <raj20@LAPTOP-Q63MOTLM>
2019-09-04 16:56:19 +0930

    Added players https://trello.com/c/m3TIqmyN/2-create-player-controller

---
+++ ProCoach/Data/Players.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCoach.Data
+{
+    public class Players
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Dob { get; set; }
+        public int Mobile_Number { get; set; }
+        public string Club_Name { get; set; }
+        public string Team { get; set; }
+        public string Game_Type { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+    }
+}
