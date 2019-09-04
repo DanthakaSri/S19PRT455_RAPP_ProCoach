62597affbcd43479e18dc5a32188d8cddce4b28b
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-09-04 16:43:46 +0930

    created practice_session class

---
+++ ProCoach/Data/practice_session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCoach.Data
+{
+    public class practice_session
+    {
+
+        public int id { get; set; }
+        public int player_id  { get; set; }
+
+        public int Coach_id { get; set; }
+
+        public int team_id { get; set; }
+        public int address { get; set; }
+    }
+}
