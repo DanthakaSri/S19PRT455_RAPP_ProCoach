f1ab7c07accb34d27f73182178eea6d0b8550a5e
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-09-09 13:54:22 +0930

    Add and update database

---
+++ ProCoach/Data/Migrations/20190909040146_procoachPracticeSession.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProCoach.Data.Migrations
+{
+    public partial class procoachPracticeSession : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "practice_session",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    team_id = table.Column<int>(nullable: false),
+                    coach_id = table.Column<int>(nullable: false),
+                    address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_practice_session", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "practice_session");
+        }
+    }
+}
