fa2dbcf7d259f3e2b27ca38536180a17e8e929db
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-09-10 10:38:31 +0930

    Created Schedule class

---
+++ ProCoach/Data/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCoach.Data
+{
+    public class Schedule
+    {
+        public int TeamId { get; set; }
+
+        public int CoachId { get; set; }
+        public DateTime Date { get; set; }
+
+        public int Time { get; set; }
+    }
+}
