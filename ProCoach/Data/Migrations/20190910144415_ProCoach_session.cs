5b236697523e4562b9ea37106b2c0f94b46d011e
raj20 <raj20@LAPTOP-Q63MOTLM>
2019-09-11 11:47:36 +0930

    Changes in player index page

---
+++ ProCoach/Data/Migrations/20190910144415_ProCoach_session.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProCoach.Data.Migrations
+{
+    public partial class ProCoach_session : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
