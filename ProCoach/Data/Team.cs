009ded3cada617a724d7f5923e064924f76ca393
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-09-16 18:10:55 +0930

    Validating the team Component https://trello.com/c/xrWfV6xE/47-validating-the-curd-functions-of-team-component

--- ProCoach/Data/Team.cs
+++ ProCoach/Data/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,15 +9,31 @@ namespace ProCoach.Data
     public class Team
     {
         public int id { get; set; }
+
+        [StringLength(10, MinimumLength = 3)]
+        [Required]
         public  string  team_name  { get; set; }
 
+
+        [Display(Name = "email")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
         public string email { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string club { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string sport { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string  coaches { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string competition { get; set; }
 
        
