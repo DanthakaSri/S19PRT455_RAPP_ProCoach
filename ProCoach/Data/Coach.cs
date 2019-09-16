c584e8918231085b1b8a0d87957d12fcbe6f8986
DanthakaSri <pasinduworks92@gmail.com>
2019-09-16 17:23:58 +0930

    Coach model validation and UI changes https://trello.com/c/36x40e7H/50-validating-the-curd-functions-for-coach-component

--- ProCoach/Data/Coach.cs
+++ ProCoach/Data/Coach.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,29 @@ namespace ProCoach.Data
     public class Coach
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        [Required]
         public string FirstName { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        [Required]
         public string LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [Required]
         public  string Dob { get; set; }
+        [Display(Name = "Mobile Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Required]
         public string Mobile { get; set; }
+
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText)]
+        [Required]
         public string Address { get; set; }
 
 
