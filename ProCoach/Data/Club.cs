ca020265791a533f31dada487e536e5e5a5c58be
ANUSHA\Anusha Lana <anusha.tng@gmail.com>
2019-09-24 18:09:10 +0930

    Validate the Create Club https://trello.com/c/i6GZll7h/61-week-8-task-5-validating-the-curd-functions-for-club-component Week 8 task 5

--- ProCoach/Data/Club.cs
+++ ProCoach/Data/Club.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,30 @@ namespace ProCoach.Data
 {
     public class Club
     {
+
         public int ID { get; set; }
 
+        [StringLength(10, MinimumLength = 3)]
+        [Required]
         public string Sport { get; set; }
 
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText)]
+        [Required]
         public string Address { get; set; }
 
+
+        [StringLength(10, MinimumLength = 10)]
+        [Display(Name = "Mobile Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Required]
         public string Phone { get; set; }
 
+
+        [StringLength(10, MinimumLength = 10)]
+        [Display(Name = "Website")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
         public string Website { get; set; }
     }
 }
