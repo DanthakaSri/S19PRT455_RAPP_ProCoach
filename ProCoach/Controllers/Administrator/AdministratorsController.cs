c774e842b5910382ea025123098655a6c7a726b4
DanthakaSri <pasinduworks92@gmail.com>
2019-10-01 21:51:03 +0930

    Redirect users based on role part 2

--- ProCoach/Controllers/Administrator/AdministratorsController.cs
+++ ProCoach/Controllers/Administrator/AdministratorsController.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProCoach.Models;
+using ProCoach.Views.Administrators;
 
 namespace ProCoach.Controllers.Administrator
 {
     public class AdministratorsController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdministratorsController(RoleManager<IdentityRole> roleManager)
+        public AdministratorsController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser>userManager )
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -35,8 +38,81 @@ namespace ProCoach.Controllers.Administrator
 
                 IdentityResult result = await _roleManager.CreateAsync(identityRole);
 
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (IdentityError error in result.Errors )
+                {
+                            ModelState.AddModelError("",error.Description);
+                }
+
             }
             return View(model);
+        }
+
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErorrMessage = $"Role with Id ={id} cannot be found";
+                return View("Not Found");
+            }
+
+            var model = new EditViewModel()
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach (var user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user,role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id );
+            if (role == null)
+            {
+                ViewBag.ErorrMessage = $"Role with Id ={model.Id} cannot be found";
+                return View("Not Found");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+
+                return View(model);
+            }
+
+
         }
     }
 }
---
+++ ProCoach/Views/Administrators/EditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProCoach.Views.Administrators
+{
+    public class EditViewModel
+    {
+        public string Id { get; set; }
+        
+        [Required(ErrorMessage = "Role name is required")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
