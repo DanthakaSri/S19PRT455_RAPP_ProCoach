4b8daac447f7767dcc0194566f369fc0dd8db366
prabhat_1234 <kprabhatrauniyar1@gmail.com>
2019-10-03 13:21:52 +0930

    homecontroller sending mail via email configuration

--- ProCoach/Controllers/HomeController.cs
+++ ProCoach/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProCoach.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System.Net.Mail;
+using System.Net;
 
 namespace ProCoach.Controllers
 {
@@ -12,14 +16,28 @@ namespace ProCoach.Controllers
     {
         public IActionResult Index()
         {
+            SendMail("subject", "body", "receivermail");
             return View();
-        }
 
+        }
+        public void SendMail(string subject, string body, string receivermail)
+        {
+
+            var client = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("kprabhatrauniyar1@gmail.com", "Scooter@1991"),
+                EnableSsl = true
+            };
+            client.Send("kprabhatrauniyar1@gmail.com", "anusha.tng@gmail.com", "HI", "This is pravat");
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
+
             return View();
+
         }
 
         public IActionResult Contact()
